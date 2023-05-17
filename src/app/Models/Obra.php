8f97e59746ea3755d3f693700e1e926c59855844
iwtapcgmr <69200078+iwtapcgmr@users.noreply.github.com>
2023-05-17 20:52:21 -0300

    Criação das models

---
+++ src/app/Models/Obra.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+
+class Obra extends Model
+{
+    protected $table = 'obra';
+}
---
+++ src/app/Models/OrcamentoItens.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrcamentoItens extends Model
+{
+    protected $table = 'orcamento_item';
+}
